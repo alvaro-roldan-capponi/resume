2cf841bb2ed26252ae788461b9aa71f42417bb50
varo.roldan <varo.roldan@gmail.com>
2023-04-23 13:38:06 -0300

    delete hardcode

--- src/app/components/html-to-pdf/html-to-pdf.component.ts
+++ src/app/components/html-to-pdf/html-to-pdf.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import jsPDF from "jspdf";
 import html2canvas from 'html2canvas';
+import * as personalData from '../../../assets/data/personal_data.json';
 
 
 @Component({
@@ -9,11 +10,12 @@ import html2canvas from 'html2canvas';
   styleUrls: ['./html-to-pdf.component.scss']
 })
 export class HtmlToPdfComponent {
-  pdf_title: string = "alvaro-roldan-capponi-cv.pdf"
+  personal_data = personalData;
 
   public async  exportPDF() {
 
     html2canvas(document.body).then(canvas => {
+      const pdfName = `${this.personal_data.name.split(" ").join("-").toLowerCase()}-cv.pdf`
 
       const doc = new jsPDF('p', 'mm');
       const contentDataURL = canvas.toDataURL('image/png');
@@ -34,7 +36,7 @@ export class HtmlToPdfComponent {
         doc.addImage(contentDataURL, 'PNG', 0, position, pageWidth, imgHeight);
         heightLeft -= pageHeight;
       }
-      doc.save(`${this.pdf_title}`);
+      doc.save(`${pdfName}`);
     });
   }
 }
